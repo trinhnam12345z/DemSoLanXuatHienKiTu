36bcd643c22c4a83f1706ec64aa54b603a07b56e
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-14 11:17:42 +0700

    Initial commit

---
+++ DemSoLanXuatHienKiTu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace DemSoLanXuatHienKiTu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string[] s = { "a", "a", "b", "c", "c" };
+            Console.WriteLine("Nhập kí tự bất kì có trong mảng: ");
+            string x = Console.ReadLine();
+            Console.WriteLine("Số lần xuất hiện của kí tự {0} là : ", x);
+            count(s, x);
+
+        }
+        static void count(string[] z, string n)
+        {
+            int dem = 0;
+            for (int i = 0; i < z.Length; i++)
+            {
+                if (z[i] == n)
+                    dem++;
+
+            }
+            Console.WriteLine(dem);
+        }
+    }
+}
